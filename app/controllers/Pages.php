b6d060bb6e805539a2d2727d17b64cb770faca76
nikocevicstefan <nikocevicstefan@gmail.com>
2019-09-15 14:42:40 +0200

    Added Core configuration

---
+++ app/controllers/Pages.php
@@ -0,0 +1,14 @@
+<?php
+
+class Pages{
+	public function __construct(){
+	}
+
+	public function index(){
+		echo "index";
+	}
+
+	public function about(){
+		echo "This is about";
+	}
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "HELLO";
+require_once '../app/bootstrap.php';
